636d636c98fd05ad74c327513b3156d35e4f3103
Sofi <Sofi.Rubizova@yandex.ru>
2023-11-24 12:47:34 +0300

    work

--- internal/storage/pg/postgres.go
+++ internal/storage/pg/postgres.go
@@ -11,10 +11,10 @@ type Postgres struct {
 }
 
 func NewStorage(ctx context.Context, dsn string) (*Postgres, error) {
-	err := migrateDatabase(dsn)
-	if err != nil {
-		return nil, fmt.Errorf("unable db migrate: %w", err)
-	}
+	//err := migrateDatabase(dsn)
+	//if err != nil {
+	//	return nil, fmt.Errorf("unable db migrate: %w", err)
+	//}
 
 	pool, err := pgxpool.New(ctx, dsn)
 	if err != nil {
@@ -23,5 +23,59 @@ func NewStorage(ctx context.Context, dsn string) (*Postgres, error) {
 	dbc := &Postgres{
 		DB: pool,
 	}
+	err = dbc.init(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("error init db: %w", err)
+	}
 	return dbc, nil
+}
+
+func (pg *Postgres) init(ctx context.Context) error {
+	tx, err := pg.DB.Begin(ctx)
+	if err != nil {
+		return err
+	}
+
+	defer tx.Rollback(ctx)
+
+	tx.Exec(ctx, `
+		CREATE TABLE "users" (
+   "id" bigserial PRIMARY KEY,
+   "login" varchar NOT NULL,
+   "balance" real NOT NULL default 0,
+   "withdrawn" real NOT NULL default 0,
+   "password" varchar NOT NULL
+);`)
+
+	tx.Exec(ctx, `
+CREATE TABLE "orders" (
+  "id" bigserial PRIMARY KEY,
+  "user_id" bigint NOT NULL REFERENCES users(id),
+  "number" varchar NOT NULL,
+  "status" varchar NOT NULL DEFAULT ('NEW'),
+  "accrual" real NOT NULL default 0,
+  "uploaded_at" timestamptz NOT NULL DEFAULT (now())
+);
+	`)
+
+	tx.Exec(ctx, `
+		CREATE TABLE IF NOT EXISTS balance(
+			id SERIAL PRIMARY KEY,
+			current NUMERIC(10,2),
+			withdrawn NUMERIC(10,2),
+			fk_user_id INTEGER REFERENCES users(id) NOT NULL
+		)
+	`)
+
+	tx.Exec(ctx, `
+CREATE TABLE "withdrawals" (
+  "id" bigserial PRIMARY KEY,
+  "user_id" bigint NOT NULL REFERENCES users(id),
+  "number" varchar NOT NULL,
+  "sum" real NOT NULL,
+  "processed_at"  timestamptz NOT NULL DEFAULT (now())
+);
+	`)
+
+	return tx.Commit(ctx)
 }
